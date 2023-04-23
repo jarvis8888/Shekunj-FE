e9826fdebe565005fbdf085aecaa5c7add25116c
shaik sameer ruddin <sameer.shaikh@protonshub.in>
2023-04-23 10:58:51 +0530

    Fixed the header

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,26 +1,113 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import "./navbar.scss";
 import closeIcon from "../../assets/icons/svgs/close.png";
 import memuIcon from "../../assets/icons/svgs/memu.png";
 import globalSreach from "../../assets/icons/svgs/globalSearch.png";
 import logo from "../../assets/icons/logo.png";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import "./index.scss";
+import { useLocation } from "react-router-dom";
+import { Menu, MenuItem } from "@mui/material";
+import { routingConstants } from "../../utils/constants";
 
-const Navbar = () => {
+const Navbar = (props) => {
   const { t } = useTranslation();
+  const history = useHistory();
+  const location = useLocation();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorE2, setAnchorE2] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [showSubMenu, setShowSubMenu] = useState(false);
+
+  const open = Boolean(anchorEl);
+  const open1 = Boolean(anchorE2);
+
+  const isActive = (href) => {
+    return location.pathname === href;
+  };
+
   return (
     <div className='Navbar'>
       <span className='nav-logo'>
         <img src={logo} alt='close' width={100} height={30} />
       </span>
       <div className={`nav-items ${isOpen && "open"}`}>
-        <a href='/about-us'> {t("header.heading.1")}</a>
+        <a href='/about-us' className={isActive("/about-us") && "active"}>
+          {t("header.heading.1")}
+        </a>
+        <a href='/courses' className={isActive("/courses") && "active"}>
+          {t("header.heading.2")}
+        </a>
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.SUCCESS_CAREER_OPTION}
+          className={
+            isActive(routingConstants.SUCCESS_CAREER_OPTION) && "active"
+          }
+        >
+          {t("headerComponent.menuItem.5")}
+        </a>
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.SUCCESS_CAREER_TEST}
+          className={isActive(routingConstants.SUCCESS_CAREER_TEST) && "active"}
+        >
+          {t("headerComponent.menuItem.6")}
+        </a>
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.GUIDANCE_BOOK}
+          className={isActive(routingConstants.GUIDANCE_BOOK) && "active"}
+        >
+          {t("headerComponent.menuItem.7")}
+        </a>
 
-        <a href='/courses'>{t("header.heading.2")}</a>
-
-        <a>{t("header.heading.3")}</a>
+        <span
+          className='subMenu-items'
+          aria-controls='basic-menu'
+          aria-haspopup='true'
+          aria-expanded={open ? "true" : undefined}
+          onClick={(event) => setAnchorEl(event.currentTarget)}
+          style={{ cursor: "pointer" }}
+        >
+          {t("header.heading.3")}
+          <Menu
+            id='basic-menu'
+            anchorEl={anchorEl}
+            open={open}
+            onClose={() => setAnchorEl(null)}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.SUCCESS_CAREER_OPTION);
+                setAnchorEl(null);
+              }}
+            >
+              {t("headerComponent.menuItem.5")}
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.SUCCESS_CAREER_TEST);
+                setAnchorEl(null);
+              }}
+            >
+              {t("headerComponent.menuItem.6")}
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.GUIDANCE_BOOK);
+                setAnchorEl(null);
+              }}
+            >
+              {t("headerComponent.menuItem.7")}
+            </MenuItem>
+          </Menu>
+        </span>
         <a
           href='https://octahire.com/Resume_maker'
           target='_blank'
@@ -28,7 +115,7 @@ const Navbar = () => {
         >
           {t("header.heading.4")}
         </a>
-        <a>{t("header.heading.5")}</a>
+
         <a
           href='https://octahire.com/Home/candidate_register'
           target='_blank'
@@ -36,10 +123,80 @@ const Navbar = () => {
         >
           {t("header.heading.6")}
         </a>
-        <a href='/success-stories'>{t("header.heading.8")}</a>
-        <span>
-          <img src={globalSreach} alt='close' />
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.TOP_COLLEGES}
+          className={isActive(routingConstants.TOP_COLLEGES) && "active"}
+        >
+          Top Colleges
+        </a>
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.TOP_SCHOOLS}
+          className={isActive(routingConstants.TOP_SCHOOLS) && "active"}
+        >
+          Top Schools
+        </a>
+        <a
+          id='subMenu-mobile'
+          href={routingConstants.GOVERNMENT_SCHEMES}
+          className={isActive(routingConstants.GOVERNMENT_SCHEMES) && "active"}
+        >
+          Government Schemes
+        </a>
+        <span
+          className='subMenu-items'
+          aria-controls='basic-menu'
+          aria-haspopup='true'
+          aria-expanded={open ? "true" : undefined}
+          onClick={(event) => setAnchorE2(event.currentTarget)}
+          style={{ cursor: "pointer" }}
+        >
+          {t("header.heading.5")}
+          <Menu
+            id='basic-menu'
+            anchorEl={anchorE2}
+            open={open1}
+            onClose={() => setAnchorE2(null)}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.TOP_COLLEGES);
+                setAnchorE2(null);
+              }}
+            >
+              Top Colleges
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.TOP_SCHOOLS);
+                setAnchorE2(null);
+              }}
+            >
+              Top Schools
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                history.push(routingConstants.GOVERNMENT_SCHEMES);
+                setAnchorE2(null);
+              }}
+            >
+              Government Schemes
+            </MenuItem>
+          </Menu>
         </span>
+        <a
+          href='/success-stories'
+          className={isActive("/success-stories") && "active"}
+        >
+          {t("header.heading.8")}
+        </a>
+        <a>
+          <img src={globalSreach} alt='close' className='search' />
+        </a>
       </div>
       <div
         className={`nav-toggle ${isOpen && "open"}`}
@@ -55,4 +212,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default memo(Navbar);
--- src/components/Header/TopNavbar.js
+++ src/components/Header/TopNavbar.js
@@ -1,34 +1,153 @@
-import React from "react";
+import React, { memo, useState } from "react";
 import ChangeLanguageButton from "../LanguageButton";
-import { Link } from "@mui/material";
+import { Avatar, Link, Menu, MenuItem } from "@mui/material";
 import { routingConstants } from "../../utils/constants";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+
 import "./navbar.scss";
 import "../../Styles/global.scss";
+import "./index.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfile, logOut, refreshPage } from "../../store/auth/action";
+import Cookies from "js-cookie";
+import { isAuthenticated } from "../../utils";
 
 function TopNavbar() {
   const { t } = useTranslation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { isAuth, user } = useSelector((state) => state.authReducer);
+  const { lan } = useSelector((state) => state.languageReducer);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [showSubMenu, setShowSubMenu] = useState(false);
+  const open = Boolean(anchorEl);
+
+
+  const handleSubmenuClick = () => {
+    setShowSubMenu(!showSubMenu);
+  };
+
+
+  const handleLogout = () => {
+    dispatch(logOut(history));
+    setAnchorEl(null);
+  };
+  React.useEffect(() => {
+    dispatch(refreshPage());
+    if (isAuthenticated()) {
+      dispatch(getUserProfile());
+    }
+  }, [dispatch, lan]);
+
+  React.useEffect(() => {
+    if (Cookies.get("sheToken") == undefined || null) {
+      localStorage.removeItem("login_data");
+      localStorage.removeItem("event_data");
+    }
+  }, []);
 
   return (
     <div className='header-top'>
       <nav>
         <ul>
           <li>
-            <a href='#'>Events</a>
+            <a href='/event'>Events</a>
           </li>
           <li>
-            <a href='#'>Blogs</a>
+            <a href='/blogs'>Blogs</a>
           </li>
           <li>
-            <a href='#'>FQA</a>
+            <a href='/more-faq'>FQA</a>
           </li>
           <li>
             <ChangeLanguageButton />
           </li>
           <li>
-            {/* <a href='/login'>
-              <button className='button'>{t("header.authButton")}</button>
-            </a> */}
+            {isAuth ? (
+              <>
+                <div className='profile'>
+                  {user && user?.profile_pic ? (
+                    <img
+                      src={user?.profile_pic}
+                      className='profile-img'
+                      alt={user?.name || t("common.n/a")}
+                    />
+                  ) : (
+                    <Avatar
+                      className='ml-xl-3 ml-md-2'
+                      style={{ cursor: "pointer" }}
+                      sx={{ width: 34, height: 34 }}
+                    />
+                  )}
+                  <span
+                    style={{
+                      lineHeight: "38px",
+                      marginLeft: "10px",
+                      cursor: "pointer",
+                    }}
+                    id='basic-button'
+                    aria-controls='basic-menu'
+                    aria-haspopup='true'
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={(event) => setAnchorEl(event.currentTarget)}
+                  >
+                    {user?.name || t("common.n/a")}&nbsp;
+                    {user && user?.last_name}
+                  </span>
+                  <Menu
+                    id='basic-menu'
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={() => setAnchorEl(null)}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  >
+                    <MenuItem
+                      onClick={() => {
+                        history.push(routingConstants.MY_PROFILE);
+                        setAnchorEl(null);
+                      }}
+                      // className={subPage === "myProfile" && "active"}
+                    >
+                      {t("headerComponent.menuItem.1")}
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        history.push(routingConstants.MY_PROGESS);
+                        setAnchorEl(null);
+                      }}
+                      // className={subPage === "myProgress" && "active"}
+                    >
+                      {t("headerComponent.menuItem.2")}
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        history.push(routingConstants.ALL_CERTIFICATE_PAGE);
+                        setAnchorEl(null);
+                      }}
+                      // className={subPage === "allCertificatePage" && "active"}
+                    >
+                      {t("headerComponent.menuItem.3")}
+                    </MenuItem>
+                    <MenuItem onClick={handleLogout}>
+                      {t("headerComponent.menuItem.4")}
+                    </MenuItem>
+                  </Menu>
+
+                </div>
+              </>
+            ) : (
+              <>
+                <a href='/login'>
+                  <button style={{ padding: "4px 10px", borderRadius: "6px" }}>
+                    {t("header.authButton")}
+                  </button>
+                </a>
+              </>
+            )}
           </li>
         </ul>
       </nav>
@@ -36,4 +155,4 @@ function TopNavbar() {
   );
 }
 
-export default TopNavbar;
+export default memo(TopNavbar);
