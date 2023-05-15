fd0d15376e6756d8a3d337f3ba017a9d229ddada
shaik sameer ruddin <sameer.shaikh@protonshub.in>
2023-05-15 13:03:59 +0530

    added the social share media to app

--- src/components/Footer/TopFooter.js
+++ src/components/Footer/TopFooter.js
@@ -8,10 +8,10 @@ export const TopFooter = memo(() => {
     <>
       <div className='wrapper'>
         <footer className='footer'>
-          <div className="container">
-            <div className="row">
+          <div className='container'>
+            <div className='row'>
               <div className='col-xl-3 col-lg-3 col-md-6 col-sm-12'>
-                <div className="sk-footer-menu">
+                <div className='sk-footer-menu'>
                   <h5>Free Courses</h5>
                   <ul>
                     <li>
@@ -36,64 +36,78 @@ export const TopFooter = memo(() => {
                 </div>
               </div>
               <div className='col-xl-3 col-lg-3 col-md-6 col-sm-12'>
-                <div className="sk-footer-menu">
+                <div className='sk-footer-menu'>
                   <h5>Popular Topics</h5>
                   <ul>
                     <li>
-                      <a href='javascript:;'>Online Mock Test</a>
+                      <a href='/mock-test/'>Online Mock Test</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Government Schemes</a>
+                      <a href='/government-schemes-in-india'>
+                        Government Schemes
+                      </a>
                     </li>
                     <li>
                       <a href='javascript:;'>Certificate</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Resume Builder</a>
+                      <a
+                        href='https://octahire.com/Resume_maker'
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Resume Builder
+                      </a>
                     </li>
                     <li>
                       <a href='javascript:;'>Guidance</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Jobs</a>
+                      <a
+                        href='https://octahire.com/Home/candidate_register'
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Jobs
+                      </a>
                     </li>
                   </ul>
                 </div>
               </div>
               <div className='col-xl-3 col-lg-3 col-md-6 col-sm-12'>
-                <div className="sk-footer-menu">
+                <div className='sk-footer-menu'>
                   <h5>About SheKunjs</h5>
                   <ul>
                     <li>
-                      <a href='javascript:;'>About Us</a>
+                      <a href='/about-us'>About Us</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Success Story</a>
+                      <a href='/success-stories'>Success Story</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Career</a>
+                      <a href='/career-options'>Career</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Blog</a>
+                      <a href='/blogs'>Blog</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Events</a>
+                      <a href='/event'>Events</a>
                     </li>
                   </ul>
                 </div>
               </div>
               <div className='col-xl-3 col-lg-3 col-md-6 col-sm-12'>
-                <div className="sk-footer-menu">
+                <div className='sk-footer-menu'>
                   <h5>Need Some Help?</h5>
                   <ul>
                     <li>
                       <a href='javascript:;'>Contact Us</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>FAQs</a>
+                      <a href='/more-faq'>FAQs</a>
                     </li>
                     <li>
-                      <a href='javascript:;'>Help & Support</a>
+                      <a href='/more-faq'>Help & Support</a>
                     </li>
                     <li>
                       <a href='javascript:;'>Terms & Policy</a>
