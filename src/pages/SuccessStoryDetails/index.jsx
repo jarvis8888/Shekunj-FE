239eb3b7bbdcf4285462dadf38f1df4a27797054
banvaritailor <banvari.tailor@protons.in>
2023-06-21 13:45:36 +0530

    story page about sec add

--- src/pages/SuccessStoryDetails/index.jsx
+++ src/pages/SuccessStoryDetails/index.jsx
@@ -16,6 +16,7 @@ import TrendingCards from "../../components/cards/TrendingCards";
 import fire from "../../assets/images/fire.svg";
 import { HashtagAndCatagories } from "../../components/HastagAndCatagories/Index";
 import hash from "../../assets/images/hashtag.svg";
+import logo from "../../assets/images/logo.svg";
 import { apiConstants, routingConstants } from "../../utils/constants";
 import httpServices from "../../utils/ApiServices";
 import { CustomLoader } from "../../components/customLoader/CustomLoader";
@@ -364,6 +365,21 @@ const SuccessStoryDetails = () => {
                           title={successStoriesDetails?.title}
                           image={successStoriesDetails?.image}
                         />
+                      </div>
+                    </div>
+                    <div className="sk-shekunj-about">
+                      <div className="row align-items-center">
+                        <div className="col-xl-3 col-lg-4 col-md-4">
+                          <div className="sk-aboutshekunj-img">
+                            <img src={logo} alt='logo'/>
+                          </div>
+                        </div>
+                        <div className="col-xl-9 col-lg-8 col-md-8">
+                          <div className="sk-shekunjAbout-content">
+                            <h5>About Shekunj</h5>
+                            <p>Shekunj promotes personal growth for women to achieve their full potential and aims to empower all the women out there - be they 16 or 60 years old - through education and guidance. Reach us at <a href="mailto:info@shekunj.com">info@shekunj.com</a></p>
+                          </div>
+                        </div>
                       </div>
                     </div>
                     <>
