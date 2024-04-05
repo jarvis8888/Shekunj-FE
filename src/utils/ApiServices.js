1d1a8235ae891c14e04d8819b6c3dbc5c931dc0c
shaik sameer ruddin <sameer.shaikh@protonshub.in>
2024-04-05 15:30:00 +0530

    added the url

--- src/utils/ApiServices.js
+++ src/utils/ApiServices.js
@@ -11,8 +11,8 @@ export const getLanguage = () => {
   return currentLang ? currentLang?.split("-")[0] : "en";
 };
 
-// const apiBaseUrl = "https://admin.shekunj.com/";
-const apiBaseUrl = "http://13.233.216.29:8000/";
+const apiBaseUrl = "https://admin.shekunj.com/";
+// const apiBaseUrl = "http://13.233.216.29:8000/";
 // const apiBaseUrl = "http://aa85-103-9-12-74.ngrok-free.app/";  
 axios.defaults.baseURL = `${apiBaseUrl}${getLanguage()}/api/`;
 
